f497059d1bebea659bebcf2784d04f2134c2c832
Mars Saxman <mars@redecho.org>
2014-11-02 18:52:23 -0800

    handle SIGINT by shutting down ncurses

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,34 @@
 #include "console.h"
 #include "browser.h"
 #include "editor.h"
+#include <signal.h>
+
+static std::unique_ptr<UI> s_ui;
+
+static void finish(int sig)
+{
+	s_ui.reset();
+	exit(0);
+}
 
 int main(int argc, char **argv)
 {
-	UI ui;
+	(void)signal(SIGINT, finish);
+	s_ui.reset(new UI);
         // Build our two starting windows.
         std::unique_ptr<Controller> console(new Console);
-        ui.open_window(std::move(console));
+        s_ui->open_window(std::move(console));
         std::unique_ptr<Controller> browser(new Browser);
-        ui.open_window(std::move(browser));
+        s_ui->open_window(std::move(browser));
 	// If we got a list of arguments, create editors for them.
 	for (int i = 1; i < argc; ++i) {
-		ui.open_window(std::unique_ptr<Controller>(new Editor(argv[i])));
+		s_ui->open_window(std::unique_ptr<Controller>(new Editor(argv[i])));
 	}
+	timeout(20);
 	do {
 		update_panels();
 		doupdate();
-	} while (ui.process(getch()));
+	} while (s_ui->process(getch()));
 	return 0;
 }
+
