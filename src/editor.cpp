67dd0aea5c029dceb1f9ac91d9c518410979fa63
Mars Saxman <mars@redecho.org>
2014-11-04 01:40:28 -0800

    editor now uses line-by-line dirty flags instead of always globally repainting

--- src/editor.cpp
+++ src/editor.cpp
@@ -14,69 +14,135 @@ Editor::Editor(std::string targetpath):
 
 void Editor::paint(WINDOW *dest, bool active)
 {
-	View view(dest);
-	size_t effscroll = std::min(maxscroll(dest), _scrollpos);
-	for (int i = 0; i < view.height(); ++i) {
-		size_t line = i + effscroll;
-		if (line < _lines.size()) {
-			view.write_line(i, _lines[line]);
-		} else {
-			view.clear_line(i);
-		}
+	update_dimensions(dest);
+	if (active != _last_active || dest != _last_dest) {
+		_update.all();
+		_last_active = active;
+		_last_dest = dest;
 	}
+	size_t effscroll = std::min(_maxscroll, _scrollpos);
+	for (unsigned i = 0; i < _height; ++i) {
+		size_t line = i + effscroll;
+		if (!_update.is_dirty(line)) continue;
+		wmove(dest, (int)i, 0);
+		if (line < _lines.size()) {
+			waddnstr(dest, _lines[line].c_str(), _width);
+		}
+		wclrtoeol(dest);
+	}
+	_update.reset();
 }
 
 bool Editor::process(WINDOW *dest, int ch, App &app)
 {
+	update_dimensions(dest);
 	switch (ch) {
-		case 258: arrow_down(dest); break;
-		case 259: arrow_up(dest); break;
-		case 338: page_down(dest); break;
-		case 339: page_up(dest); break;
+		case KEY_DOWN: cursor_vert(1); break;
+		case KEY_UP: cursor_vert(-1); break;
+		case KEY_LEFT: arrow_left(); break;
+		case KEY_RIGHT: arrow_right(); break;
+		case 338: cursor_vert(_halfheight); break;
+		case 339: cursor_vert(-_halfheight); break;
 		default: break;
+	}
+	if (_update.has_dirty()) {
+		paint(dest, true);
 	}
 	return true;
 }
 
-void Editor::arrow_down(WINDOW *dest)
+void Editor::arrow_left()
 {
-	if (_scrollpos < maxscroll(dest)) {
-		_scrollpos++;
-		paint(dest, true);
+}
+
+void Editor::arrow_right()
+{
+}
+
+void Editor::Update::reset()
+{
+	_dirty = false;
+	_linestart = 0;
+	_lineend = 0;
+}
+
+void Editor::Update::all()
+{
+	_dirty = true;
+	_linestart = 0;
+	_lineend = (size_t)-1;
+}
+
+void Editor::Update::line(size_t line)
+{
+	_linestart = _dirty ? std::min(_linestart, line) : line;
+	_lineend = _dirty ? std::max(_lineend, line) : line;
+	_dirty = true;
+}
+
+void Editor::Update::range(size_t a, size_t b)
+{
+	size_t from = std::min(a, b);
+	size_t to = std::max(a, b);
+	_linestart = _dirty ? std::min(_linestart, from) : from;
+	_lineend = _dirty ? std::max(_linestart, to) : to;
+	_dirty = true;
+}
+
+bool Editor::Update::is_dirty(size_t line) const
+{
+	return _dirty && (line >= _linestart) && (line <= _lineend);
+}
+
+bool Editor::line_visible(size_t index) const
+{
+	return index >= _scrollpos && (index - _scrollpos) < _height;
+}
+
+void Editor::reveal_cursor()
+{
+	// If the cursor is already on screen, do nothing.
+	if (line_visible(_cursy)) return;
+	// Try to center the viewport over the cursor.
+	if (_cursy < _scrollpos) {
+		_scrollpos = _cursy - std::min(_cursy, _halfheight);
+	}
+	// Don't scroll so far we reveal empty space.
+	_scrollpos = std::min(_scrollpos, _maxscroll);
+	_update.all();
+}
+
+void Editor::cursor_vert(int delta)
+{
+	size_t cursy = _cursy;
+	if (delta > 0) {
+		cursy = std::min(cursy + 1, _maxscroll);
+	} else if (cursy > 0) {
+		cursy += delta;
+	}
+	if (cursy != _cursy) {
+		_update.range(_cursy, cursy);
+		reveal_cursor();
 	}
 }
 
-void Editor::arrow_up(WINDOW *dest)
+void Editor::update_dimensions(WINDOW *view)
 {
-	size_t minscroll = 0;
-	if (_scrollpos > minscroll) {
-		_scrollpos--;
-		paint(dest, true);
+	int height, width;
+	getmaxyx(view, height, width);
+	if ((size_t)height != _height) {
+		_height = (size_t)height;
+		_halfheight = _height / 2;
+		_update.all();
 	}
-}
-
-void Editor::page_down(WINDOW *dest)
-{
-	View view(dest);
-	size_t step = view.height() / 2;
-	_scrollpos = std::min(_scrollpos + step, maxscroll(dest));
-	paint(dest, true);
-}
-
-void Editor::page_up(WINDOW *dest)
-{
-	View view(dest);
-	size_t step = view.height() / 2;
-	_scrollpos = std::max((int)_scrollpos - (int)step, 0);
-	paint(dest, true);
-}
-
-size_t Editor::maxscroll(WINDOW *dest)
-{
-	View view(dest);
-	if (_lines.size() > (size_t)view.height()) {
-		return _lines.size() - view.height();
-	} else {
-		return 0;
+	if ((size_t)width != _width) {
+		_width = (size_t)width;
+		_update.all();
+	}
+	size_t linecount = _lines.size();
+	size_t newmax = linecount > _height ? linecount - _height : 0;
+	if (newmax != _maxscroll) {
+		_maxscroll = newmax;
+		_update.all();
 	}
 }
--- src/editor.h
+++ src/editor.h
@@ -13,15 +13,43 @@ public:
 	virtual bool poll(WINDOW *view, App &app) override { return true; }
 	virtual std::string title() const override { return _targetpath; }
 protected:
-	void arrow_up(WINDOW *view);
-	void arrow_down(WINDOW *view);
-	void page_up(WINDOW *view);
-	void page_down(WINDOW *view);
-	size_t maxscroll(WINDOW *view);
+	void arrow_left();
+	void arrow_right();
+
+	class Update
+	{
+	public:
+		void reset();
+		void all();
+		void line(size_t line);
+		void range(size_t from, size_t to);
+		bool has_dirty() const { return _dirty; }
+		bool is_dirty(size_t line) const;
+	private:
+		bool _dirty = true;
+		size_t _linestart = 0;
+		size_t _lineend = 0;
+	} _update;
+
+	bool line_visible(size_t index) const;
+	void reveal_cursor();
+	void cursor_vert(int delta);
+	void update_dimensions(WINDOW *view);
 private:
 	std::string _targetpath;
 	std::vector<std::string> _lines;
+	bool _last_active = false;
+	WINDOW *_last_dest = nullptr;
+	// What are the width and height of the viewrect?
+	size_t _width = 0;
+	size_t _height = 0;
+	size_t _halfheight = 0;
+	size_t _maxscroll = 0;
+	// What is the vertical position of the viewrect?
 	size_t _scrollpos = 0;
+	// Where is the cursor located within the document?
+	size_t _cursy = 0;
+	size_t _cursx = 0;
 };
 
 #endif // CONSOLE_H
