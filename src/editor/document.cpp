22d67e3036d76be7e273e2d771a6b9d978ca98cb
Mars Saxman <mars@redecho.org>
2014-11-04 16:57:51 -0800

    let's give these types some more descriptive names

--- src/editor/document.cpp
+++ src/editor/document.cpp
@@ -12,44 +12,44 @@ Editor::Document::Document(std::string targetpath)
 	}
 }
 
-std::string Editor::Document::get_line_text(size_t index) const
+std::string Editor::Document::get_line_text(line_t index) const
 {
 	// get the specified line, if it is in range, or the empty string
 	// if that is what we should display instead
 	return index < _lines.size() ? _lines[index] : "";
 }
 
-size_t Editor::Document::get_line_size(size_t index) const
+size_t Editor::Document::get_line_size(line_t index) const
 {
 	// return the line's length in chars, or 0 if out of range
 	return index < _lines.size() ? _lines[index].size() : 0;
 }
 
-size_t Editor::Document::char_for_column(unsigned column, size_t line) const
+Editor::Document::offset_t Editor::Document::char_for_column(column_t h, line_t index) const
 {
 	// Given a screen column coordinate and a line number,
 	// compute the character offset which most closely
 	// precedes that column.
-	std::string text = get_line_text(line);
-	size_t charoff = 0;
-	unsigned xpos = 0;
+	std::string text = get_line_text(index);
+	offset_t charoff = 0;
+	column_t xpos = 0;
 	for (auto ch: text) {
 		xpos += char_width(ch, xpos);
-		if (xpos >= column) break;
+		if (xpos >= h) break;
 		charoff++;
 	}
 	return charoff;
 }
 
-unsigned Editor::Document::column_for_char(size_t charoff, size_t line) const
+Editor::Document::column_t Editor::Document::column_for_char(offset_t charoff, line_t index) const
 {
 	// Given a character offset and a line number, compute
 	// the screen column coordinate where that character
 	// should appear.
-	std::string text = get_line_text(line);
+	std::string text = get_line_text(index);
 	charoff = std::min(charoff, text.size());
-	unsigned column = 0;
-	size_t charpos = 0;
+	column_t column = 0;
+	offset_t charpos = 0;
 	for (auto ch: text) {
 		charpos++;
 		if (charpos >= charoff) break;
@@ -58,14 +58,14 @@ unsigned Editor::Document::column_for_char(size_t charoff, size_t line) const
 	return column;
 }
 
-unsigned Editor::Document::char_width(char ch, size_t column) const
+unsigned Editor::Document::char_width(char ch, column_t column) const
 {
 	// How many columns wide is this character, when
 	// it appears at the specified column?
 	return (ch == '\t') ? tab_width(column) : 1;
 }
 
-unsigned Editor::Document::tab_width(size_t column) const
+unsigned Editor::Document::tab_width(column_t column) const
 {
 	// How many columns wide is a tab character
 	// which begins at the specified column?
--- src/editor/document.h
+++ src/editor/document.h
@@ -11,18 +11,23 @@ namespace Editor {
 class Document
 {
 public:
+	typedef size_t line_t;
+	typedef size_t offset_t;
+	typedef unsigned row_t;
+	typedef unsigned column_t;
+
 	Document(std::string targetpath);
 	static const unsigned kTabWidth;
-	size_t maxline() const { return _maxline; }
-	std::string get_line_text(size_t index) const;
-	size_t get_line_size(size_t index) const;
-	size_t char_for_column(unsigned column, size_t line) const;
-	unsigned column_for_char(size_t charoff, size_t line) const;
-	unsigned char_width(char ch, size_t column) const;
-	unsigned tab_width(size_t column) const;
+	line_t maxline() const { return _maxline; }
+	std::string get_line_text(line_t index) const;
+	size_t get_line_size(line_t index) const;
+	offset_t char_for_column(column_t h, line_t index) const;
+	column_t column_for_char(offset_t c, line_t index) const;
+	unsigned char_width(char ch, column_t h) const;
+	unsigned tab_width(column_t h) const;
 private:
 	std::vector<std::string> _lines;
-	size_t _maxline = 0;	// ubound, not size
+	line_t _maxline = 0;	// ubound, not size
 };
 } // namespace Editor
 
