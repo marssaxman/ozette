d331dee28739c04e9146391842ee27387b239f47
Mars Saxman <mars@redecho.org>
2014-11-27 00:06:53 -0800

    copy stock configuration files into /usr/share

--- install.sh
+++ install.sh
@@ -5,6 +5,11 @@
 # We expect that Lindi has already been built successfully.
 
 cp lindi /usr/bin/lindi
+if [[ -e /usr/share/lindi ]]
+then
+	rm -rf /usr/share/lindi
+fi
+cp -R ../share /usr/share/lindi
 which lindi
 
 
