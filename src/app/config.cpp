34cecfdbbc5e708fea7b4a5dc465d6c64a7ec448
Mars Saxman <mars@redecho.org>
2014-12-08 18:36:23 -0800

    the settings object lived at the wrong level; reworked

---
+++ src/app/config.cpp
@@ -0,0 +1,106 @@
+//
+// lindi
+// Copyright (C) 2014 Mars J. Saxman
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#include "config.h"
+#include "INIReader.h"
+
+Config::All::All(std::string confdir, std::string workingdir):
+	_user(new INIReader(confdir + "/config")),
+	_dir(new INIReader(workingdir + "/.lindi/config"))
+{
+}
+
+void Config::All::change_dir(std::string workingdir)
+{
+	_dir.reset(new INIReader(workingdir + "/.lindi/config"));
+}
+
+std::string Config::All::get(std::string key, std::string val) const
+{
+	return get("", key, val);
+}
+
+int Config::All::get_int(std::string key, int val) const
+{
+	return get_int("", key, val);
+}
+
+bool Config::All::get_bool(std::string key, bool val) const
+{
+	return get_bool("", key, val);
+}
+
+std::string Config::All::get(
+		std::string group, std::string key, std::string val) const
+{
+	if (!_user->ParseError()) {
+		val = _user->Get(group, key, val);
+	}
+	if (!_dir->ParseError()) {
+		val = _dir->Get(group, key, val);
+	}
+	return val;
+}
+
+int Config::All::get_int(std::string group, std::string key, int val) const
+{
+	if (!_user->ParseError()) {
+		val = (int)_user->GetInteger(group, key, (long)val);
+	}
+	if (!_dir->ParseError()) {
+		val = (int)_dir->GetInteger(group, key, (long)val);
+	}
+	return val;
+}
+
+bool Config::All::get_bool(std::string group, std::string key, bool val) const
+{
+	if (!_user->ParseError()) {
+		val = _user->GetBoolean(group, key, val);
+	}
+	if (!_dir->ParseError()) {
+		val = _dir->GetBoolean(group, key, val);
+	}
+	return val;
+}
+
+Config::Typed::Typed(const All &source, std::string filepath):
+	_source(source),
+	_type(filepath.substr(filepath.find_last_of('.')))
+{
+}
+
+std::string Config::Typed::get(std::string key, std::string val) const
+{
+	val = _source.get(key, val);
+	return _source.get(_type, key, val);
+}
+
+int Config::Typed::get_int(std::string key, int val) const
+{
+	val = _source.get_int(key, val);
+	return _source.get_int(_type, key, val);
+}
+
+bool Config::Typed::get_bool(std::string key, bool val) const
+{
+	val = _source.get_bool(key, val);
+	return _source.get_bool(_type, key, val);
+}
+
---
+++ src/app/config.h
@@ -0,0 +1,60 @@
+//
+// lindi
+// Copyright (C) 2014 Mars J. Saxman
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <string>
+#include <memory>
+
+class INIReader;
+namespace Config {
+
+// Configuration settings which apply in all contexts.
+class All
+{
+public:
+	All(std::string confdir, std::string workingdir);
+	void change_dir(std::string workingdir);
+	std::string get(std::string key, std::string def_val) const;
+	int get_int(std::string key, int def_val) const;
+	bool get_bool(std::string key, bool def_val) const;
+	std::string get(std::string group, std::string key, std::string val) const;
+	int get_int(std::string group, std::string key, int val) const;
+	bool get_bool(std::string group, std::string key, bool val) const;
+private:
+	std::unique_ptr<INIReader> _user;
+	std::unique_ptr<INIReader> _dir;
+};
+
+// Configurations which may be specific to a certain file's type.
+class Typed
+{
+public:
+	Typed(const All &source, std::string filepath);
+	std::string get(std::string key, std::string def_val) const;
+	int get_int(std::string key, int def_val) const;
+	bool get_bool(std::string key, bool def_val) const;
+private:
+	const All &_source;
+	std::string _type;
+};
+} // namespace Config
+
+#endif //CONFIG_H
