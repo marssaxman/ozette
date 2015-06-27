4c40dc33c7c4ccf524d3ea297864fae37d759b7c
Mars Saxman <mars@redecho.org>
2015-06-27 10:58:44 -0700

    result overlay no longer hides cursor in editor window

--- src/ui/window.cpp
+++ src/ui/window.cpp
@@ -266,7 +266,7 @@ void UI::Window::paint_content()
 {
 	// We must draw the content from back to front: view, result, dialog.
 	View::State content_state = View::State::Focused;
-	if (_dialog || !_result_text.empty()) {
+	if (_dialog) {
 		content_state = View::State::Active;
 	}
 	if (!_has_focus) {
